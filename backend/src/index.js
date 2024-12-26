5b230f101823459c00f0165b11d8e66019585ec2
Sathvik <sathvikgowda282@gmail.com>
2024-12-26 16:15:27 +0530

    message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app=express();
 const port=process.env.PORT;
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(port, ()=>{
     console.log(`server is running on port: ${port}`);
