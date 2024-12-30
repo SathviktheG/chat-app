dc11a68b46338d91625273f122b7c8803d50a1e3
Sathvik <sathvikgowda282@gmail.com>
2024-12-30 13:16:21 +0530

    fixing change

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,11 +27,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
     })
 }
+console.log(path.resolve(__dirname, "frontend", "dist", "index.html"));
 
 server.listen(port, ()=>{
     console.log(`server is running on port: ${port}`);
